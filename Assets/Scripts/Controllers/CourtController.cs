38949630cbd5f8a560759b5034b17e857b77e209
Bowen <shiggitymiggity@gmail.com>
2023-09-26 22:54:19 -0700

    Beggar event working with choices!

--- Assets/Scripts/Controllers/CourtController.cs
+++ Assets/Scripts/Controllers/CourtController.cs
@@ -23,6 +23,10 @@ public class CourtController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _dialogueText;
     [SerializeField] private GameObject _continueDialogueButton;
 
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject[] _choices;
+    private TextMeshProUGUI[] _choicesText;
+
     private Story _currentStory;
 
     private bool _dialogueIsPlaying;
@@ -32,6 +36,8 @@ public class CourtController : MonoBehaviour
         _playerKingdom = _playerKingdom.GetComponent<PlayerKingdom>();
         _dialogueIsPlaying = false;
         _dialoguePanel.SetActive(false);
+
+        NewChoices();
     }
 
     private void Update()
@@ -92,11 +98,57 @@ public class CourtController : MonoBehaviour
         // if the story can continue then keep going else exit dialogue
         if (_currentStory.canContinue)
         {
+            // set text for current dialogue
             _dialogueText.text = _currentStory.Continue();
+            // display choices, if any, for this dialogue line
+            DisplayChoices();
         }
         else
         {
             StartCoroutine(ExitDialogueMode());
         }
+    }
+
+    private void NewChoices()
+    {
+        _choicesText = new TextMeshProUGUI[_choices.Length];
+        int idx = 0;
+        foreach (GameObject choice in _choices)
+        {
+            _choicesText[idx] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            idx++;
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = _currentStory.currentChoices;
+
+        // check that the UI has enough preset buttons to handle the choices.
+        if (currentChoices.Count > _choices.Length)
+        {
+            Debug.LogError("More choices were given than the UI can support. Number of choices given: " + currentChoices.Count);
+        }
+
+        int idx = 0;
+
+        // enable and initialize the choices up to the amount of choices for this line of dialogue
+        foreach(Choice choice in currentChoices)
+        {
+            _choices[idx].gameObject.SetActive(true);
+            _choicesText[idx].text = choice.text;
+            idx++;
+        }
+        // go through remaining choice buttons and hide them
+        for (int i = idx; i < _choices.Length; i++)
+        {
+            _choices[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        _currentStory.ChooseChoiceIndex(choiceIndex);
+        ContinueStory();
     }
 }
--- Assets/Scripts/CourtEvents/Beggar.ink
+++ Assets/Scripts/CourtEvents/Beggar.ink
@@ -1,2 +1,19 @@
+-> main
+
+=== main ===
 A man in ragged clothing stands before you.
 "Please spare some gold Sire, I cannot afford food."
+    + [Gold]
+        -> something("gold")
+    + [Nothing]
+        -> nothing("the finger")
+        
+=== something(stuff) ===
+You gave him {stuff}.
+This bro is gunna go buy a sandwich now.
+-> END
+
+=== nothing(nada) ===
+You gave him {nada}!
+Homedude is going to starve now.
+-> END
