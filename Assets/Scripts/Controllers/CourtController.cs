dc900cf2e427ce6a4e2909964b650424e732503d
Bowen <shiggitymiggity@gmail.com>
2023-09-26 21:52:49 -0700

    Beggar story is playing without choices

--- Assets/Scripts/Controllers/CourtController.cs
+++ Assets/Scripts/Controllers/CourtController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Ink.Runtime;
 
 public class CourtController : MonoBehaviour
 {
@@ -10,12 +11,40 @@ public class CourtController : MonoBehaviour
     [SerializeField] private TMP_InputField _inputText;
     [SerializeField] private TMP_Text _outputText;
     [SerializeField] private GameObject _enterButton;
+    [SerializeField] private GameObject _nextButton;
     [SerializeField] private string _storedText;
     public bool isActive { get; set; }
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset _inkJSON;
+
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject _dialoguePanel;
+    [SerializeField] private TextMeshProUGUI _dialogueText;
+    [SerializeField] private GameObject _continueDialogueButton;
+
+    private Story _currentStory;
+
+    private bool _dialogueIsPlaying;
 
     private void Start()
     {
         _playerKingdom = _playerKingdom.GetComponent<PlayerKingdom>();
+        _dialogueIsPlaying = false;
+        _dialoguePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // return right away if dialogue isn't playing
+        if (!_dialogueIsPlaying)
+        {
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            ContinueStory();
+        }
     }
 
     public void PushText(string text)
@@ -32,5 +61,42 @@ public class CourtController : MonoBehaviour
     public void TargetInputTextBox()
     {
         _inputText.Select();
+    }
+
+    public void StartDialogue()
+    {
+        // Debug.Log(_inkJSON.text);
+
+        _currentStory = new Story(_inkJSON.text);
+        _dialogueIsPlaying = true;
+        _dialoguePanel.SetActive(true);
+        _continueDialogueButton.SetActive(true);
+        _nextButton.SetActive(false);
+
+        ContinueStory();
+    }
+
+    private IEnumerator ExitDialogueMode()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        _dialogueIsPlaying = false;
+        _continueDialogueButton.SetActive(false);
+        _dialoguePanel.SetActive(false);
+        _dialogueText.text = "";
+        _nextButton.SetActive(true);
+    }
+
+    public void ContinueStory()
+    {
+        // if the story can continue then keep going else exit dialogue
+        if (_currentStory.canContinue)
+        {
+            _dialogueText.text = _currentStory.Continue();
+        }
+        else
+        {
+            StartCoroutine(ExitDialogueMode());
+        }
     }
 }
---
+++ Assets/Scripts/CourtEvents/Beggar.ink
@@ -0,0 +1,2 @@
+A man in ragged clothing stands before you.
+"Please spare some gold Sire, I cannot afford food."
